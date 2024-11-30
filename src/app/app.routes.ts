385b08ef10acb631d430eea4496a1f773f44038b
fyambos <feliciteyambos@gmail.com>
2024-11-30 20:43:19 +0100

    feat: add home and polling components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PollingComponent } from './components/polling/polling.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'polling', component: PollingComponent },
+  ];
---
+++ src/app/components/polling/polling.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-polling',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './polling.component.html'
+})
+export class PollingComponent {
+
+}
